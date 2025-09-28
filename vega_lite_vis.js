83ee2579d4a22da8fb118f8bc9c543b6314689d1
angu0160 <angu0160@student.monash.edu>
2025-09-28 23:25:01 +1000

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,7 @@
-var vg_1 = "migrant_origin.vg.json";
+var vg_1 = "migrant_origin.vl.json";
 
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
     // Access the Vega view instance
     // (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
